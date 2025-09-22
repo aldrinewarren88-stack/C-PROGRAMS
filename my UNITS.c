5ce62750c45bdeb5ee8ab20d05edc83b8d05b199
aldrinewarren88-stack <aldrinewarren88@gmail.com>
2025-09-22 19:20:10 +0300

    Add files via upload

---
+++ my UNITS.c
@@ -0,0 +1,20 @@
+/*
+ NAME:ALDRINE WARREN
+ REG NO :PA106/G/28818/25
+ DESCRIPTION : PROGRAM TO DISPLAY MY SOFTWARE ENGINEERING UNITS
+ */
+#include <stdio.h>
+int main() {
+ printf("Below are the seven units under Software Engineering year 1 semester 1\n\n");
+ printf("1.INTRODUCTION TO PROGRAMMING AND PROBLEM SOLVING\n");
+ printf("2.DISCRETE MATHEMATICS \n");
+ printf("3.MATHEMATICS FOR SCIENCE\n");
+ printf("4.COMMUNICATION SKILLS\n");
+ printf("5.FUNDAMENTALS OF ICT\n");
+ printf("6.FUNDAMENTALS OF SYSTEM ANALYSIS AND DESIGN\n");
+ printf("7.COMPUTER SYSTEMS AND ORGANISATION\n");
+
+ return 0;
+
+
+}
